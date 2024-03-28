c87bb090bf2ca45272b91ea684f515f64e302bad
Pushkar Borkar <pushkarborkar1809@gmail.com>
2024-03-28 16:49:06 +0530

    feat: make the portfolio responsive for all screens

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -29,17 +29,19 @@ const Skills = ({id}: {id: string}) => {
 
 	return (
 		<Layout id={id} icon={shapes} title="my toolbox.">
-			<div className="flex px-8 mt-5">
+			<div className="grid grid-cols-2 md:grid-cols-4 px-3 md:px-10 mt-5 text-left w-full gap-3 md:mt-5 lg:mx-0 lg:pl-1">
 				<SkillsList title="Frontend" skills={frontend} />
 				<SkillsList title="Backend" skills={backend} />
 				<SkillsList title="Databases" skills={database} />
 				<SkillsList title="Web3" skills={web3} />
 			</div>
-			<div className="px-10 mt-2">
+			<div className="ml-3 mt-2 text-left md:ml-10 lg:ml-3 lg:pl-1">
 				I also have experience with{" "}
 				<span className="font-semibold">{misc[0].name}</span>{" "}
 				{misc.slice(1, misc.length - 1).map((skill: Skill, index: number) => (
-					<span key={index} className="font-semibold">, {skill.name}</span>
+					<span key={index} className="font-semibold">
+						, {skill.name}
+					</span>
 				))}
 				{" &"}
 				<span className="font-semibold"> {misc[misc.length - 1].name}</span>
